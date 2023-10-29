3fee3bd597b728bf7e92bd0bc095e477efba8924
sirojiddin <mirzayevsky@gmail.com>
2023-10-29 12:45:54 +0500

    loader added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React,{useState} from "react";
 import Footer from "./components/Footer";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Success from "./pages/Success";
-
+import Loader from "./components/Loader/index"
 
 function App() {
-
+const [loader, setLoader] = useState(true)
+setTimeout(()=>{
+setLoader(false)
+},4000)
   
   return (
     <>
-               <BrowserRouter>
+           {
+
+
+loader === true ? <Loader/> : (
+<>
+<BrowserRouter>
                    <Routes>
                        <Route path={"/"} element={<Home/>}/>
                        <Route path="/success" element={ <Success/>} />
                    </Routes>
                </BrowserRouter>
-          <Footer />
+                <Footer />
+</>
+)
+           }     
+
          
-      
+               
     </>
   );
 }
